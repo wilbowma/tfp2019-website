3f7ff082ad11202495920c23200c656783b6d019
William J. Bowman <wjb@williamjbowman.com>
2018-12-18 18:26:03 -0800

    Added calendar

--- calendar.rkt
+++ calendar.rkt
@@ -9,18 +9,17 @@
 ;; A Date is a
 ;; Title-String x Date-String
 
-(define display-calendar? #f)
+(define display-calendar? #t)
 
 (define calendar
-  `(("Submission TBD"))
-  #;`(("Submission" "March 32, -2019")
-    ("Reviews" "March 32, -2019")
-    ("Next Submission" "March 32, -2019")
-    ("Reviews" "March 32, -2019")
-    ("Symposium" "March 32, -2019")
-    ("Post-Submission" "March 32, -2019")
-    ("Post-Reviews" "March 32, -2019")
-    ("Final" "March 32, -2019")))
+  `(("Submission Deadline" "Thursday, March 28, 2019")
+    ("Paper Notification" "Thursday, May 2, 2019")
+    ("<a href=\"http://www.staff.science.uu.nl/~hage0101/tfpie2019/index.html\">TFPIE</a>" "Tuesday, June 11, 2019")
+    ("Symposium" "Wednesday, June 12, 2019 – Friday, June 14, 2019")
+    ("Student Paper Feedback" "Friday June 21, 2019")
+    ("Submission for Formal Review" "Thursday, August 1, 2019")
+    ("Notification of Acceptance" "Thursday, October 24, 2019")
+    ("Camera Ready" "Saturday, November 30, 2019")))
 
 (define (render-calendar-row title date)
   @list{<tr>
